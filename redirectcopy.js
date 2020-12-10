98a4f1a9ab82e32390ec7e1ca65738733e83ea32
Le Tuan <35622642+nauthnael@users.noreply.github.com>
2020-12-10 20:56:20 +0700

    Add files via upload

---
+++ redirectcopy.js
@@ -0,0 +1,17 @@
+function FSAddOriginalLink(){
+    var body_element = document.getElementsByTagName('body')[0];
+    var selection;
+    selection = window.getSelection();
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://hungthinhrealty.com'>http://hungthinhrealty.com"+document.location.pathname+"</a>";
+    var copytext = pagelink;
+    var newdiv = document.createElement('div');
+    newdiv.style.position='absolute';
+    newdiv.style.left='-99999px';
+    body_element.appendChild(newdiv);
+    newdiv.innerHTML = copytext;
+    selection.selectAllChildren(newdiv);
+    window.setTimeout(function() {
+        body_element.removeChild(newdiv);
+    },0);
+}
+document.oncopy = FSAddOriginalLink;
