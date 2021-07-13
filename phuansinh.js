799c0484164f19f3d0f1db0169d309b40ecb7838
Le Tuan <35622642+nauthnael@users.noreply.github.com>
2021-07-13 14:59:24 +0700

    Update phuansinh.js

--- phuansinh.js
+++ phuansinh.js
@@ -1,12 +1,12 @@
-var listurl = [
-  "bit.ly/3kfz8Nc",
-  "bit.ly/3i9wfLs",
-  "bit.ly/3i0yD73",
-  "bit.ly/2VAtI5g",
-  "bit.ly/3AThQLM"
+var listUrlpp = [
+  'https://bit.ly/3kfz8Nc',
+  'https://bit.ly/3i9wfLs',
+  'https://bit.ly/3i0yD73',
+  'https://bit.ly/2VAtI5g',
+  'https://bit.ly/3AThQLM'
 ];
 
-var url_popup = answers[Math.floor(Math.random() * listurl.length)];
+var url_popup = _.sample(listUrlpp);
       //<![CDATA[
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
