3328d6bc8ac79a8ede5182a506b6b26a73f9eff9
Le Tuan <35622642+nauthnael@users.noreply.github.com>
2021-07-13 13:24:58 +0700

    Update loadinfo.js

--- loadinfo.js
+++ loadinfo.js
@@ -1,9 +1,9 @@
 var listUrlpp = [
-  "https://bit.ly/3kfz8Nc",
-  "https://bit.ly/3i9wfLs",
-  "https://bit.ly/3i0yD73",
-  "https://bit.ly/2VAtI5g",
-  "https://bit.ly/3AThQLM"
+  'https://bit.ly/3kfz8Nc',
+  'https://bit.ly/3i9wfLs',
+  'https://bit.ly/3i0yD73',
+  'https://bit.ly/2VAtI5g',
+  'https://bit.ly/3AThQLM'
 ];
 
 var url_popup = answers[Math.floor(Math.random() * listUrlpp.length)];
