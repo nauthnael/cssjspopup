94b6459fd4a4f0c53b6866b90b24ac7ac94519a6
NguyenQuangTrinh <58320738+NguyenQuangTrinh@users.noreply.github.com>
2022-06-06 14:55:25 +0700

    new finalinfo

---
+++ new-finalinfo.js
@@ -0,0 +1,65 @@
+var listurl = [
+    "bit.ly/3kfz8Nc",
+    "bit.ly/3i9wfLs",
+    "bit.ly/3i0yD73",
+    "bit.ly/2VAtI5g",
+    "bit.ly/3AThQLM",
+  ];
+  
+  var state = 0;
+  
+  var url_popup = Math.floor(Math.random() * listurl.length);
+  //<![CDATA[
+  function addEvent(obj, eventName, func) {
+    if (obj.attachEvent) {
+      obj.attachEvent("on" + eventName, func);
+    } else if (obj.addEventListener) {
+      obj.addEventListener("click", () => {
+        if (state >= 3) {
+          var params =
+            "height=" +
+            1 +
+            ",width=" +
+            1 +
+            ",left=9999,top=9999,location=0,toolbar=0,status=0,menubar=0,scrollbars=0,resizable=0";
+          if (document.cookie.indexOf("popup") == -1) {
+            var w = window.open(url_popup, "popup", params);
+            if (w) {
+              document.cookie = "popuphvfs=popup";
+              w.blur();
+            }
+            window.focus();
+          }
+        }else{
+          state++;
+        }
+      });
+    } else {
+      obj["on" + eventName] = func;
+    }
+  }
+  addEvent(window, "load", function (e) {
+    addEvent(document.body, "click", function (e) {
+      console.log("click");
+    //   if (state >= 3) {
+    //     console.log("true");
+    //     var params =
+    //       "height=" +
+    //       1 +
+    //       ",width=" +
+    //       1 +
+    //       ",left=9999,top=9999,location=0,toolbar=0,status=0,menubar=0,scrollbars=0,resizable=0";
+    //     if (document.cookie.indexOf("popup") == -1) {
+    //       var w = window.open(url_popup, "popup", params);
+    //       if (w) {
+    //         document.cookie = "popuphvfs=popup";
+    //         w.blur();
+    //       }
+    //       window.focus();
+    //     }
+    //   } else {
+    //   }
+    });
+  });
+  //]]>
+  
